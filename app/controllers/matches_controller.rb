7673ca86a10ffe2f52c62df4b49a3670fe91c01d
yakko-chn <o.ub5ryno16i@gmail.com>
2021-02-09 00:37:31 +0900

    new/createアクション

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,23 @@
 class MatchesController < ApplicationController
   def index
   end
+
+  def new
+   @match = Match.new
+  end
+
+  def create
+   @match = Match.new(match_params)
+
+    if @match.save
+     redirect_to root_path
+    else
+     render :new
+    end
+  end
+
+  private
+   def match_params
+    params.require(:match).permit(:room_id, :inning,:speed,:retire,:spirits) 
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   root to: 'matches#top'
-  resources :items
+  resources :matches
 
 end
