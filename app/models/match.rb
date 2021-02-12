7cdb7960ed75b5434e9d1901fe2fbae6ddb7ad52
yakko-chn <o.ub5ryno16i@gmail.com>
2021-02-12 22:29:17 +0900

    validation設定

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,9 +1,9 @@
 class Match < ApplicationRecord
 
+# ルームIDは7桁
   with_options presence: true do
     validates :room_id, length: { maximum: 7, minimum: 7 }, uniqueness: true
   end
-
 
 
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # ユーザー名の制約
+    with_options presence: true do
+      validates :name, length: { maximum: 18 }, uniqueness: true
+    end
+  
+  # メールアドレスの制約
+    VALID_EMAIL = /\A\S+@\S+\.\S+\z/.freeze
+    validates :email, uniqueness: true, format: { with: VALID_EMAIL }
+  
+  # パスワード設定の制約
+    VALID_PASSWORD = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates  :password, confirmation: true, format: { with: VALID_PASSWORD }
+    validates  :password_confirmation, presence: true
 
 
   has_many :matches
